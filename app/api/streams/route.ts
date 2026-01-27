aec37587e142a16c81fae635d308b5ceca06afc0
Zenon <abhishekyadav276208@gmail.com>
2026-01-27 16:48:02 +0530

    shadcn init

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -5,6 +5,7 @@ import { prismaClient } from '../lib/db';
 
 const YT_REGEX = /^(?:(?:https?:)?\/\/)?(?:www\.)?(?:m\.)?(?:youtu(?:be)?\.com\/(?:v\/|embed\/|watch(?:\/|\?v=))|youtu\.be\/)((?:\w|-){11})(?:\S+)?$/
 //spotify regex can be added later
+
 const CreateStreamSchema = z.object({
 	createrId: z.string(),
 	url: z.string() //contain only youtube or spotify url
@@ -27,20 +28,23 @@ export async function POST(req: NextRequest) {
 		const res = await fetch(`https://www.googleapis.com/youtube/v3/videos?key=${process.env.YTAPI}&part=snippet&id=${extractedId}`)
 		const json = await res.json()
 
-		console.log(json.items?.[0]?.snippet?.title)
-		console.log(json.items?.[0]?.snippet.thumbnails)
+		const title = json.items?.[0]?.snippet?.title as string
+		const tb = json.items?.[0]?.snippet.thumbnails
 
 		const stream = await prismaClient.stream.create({
 			data: {
 				userId: data.createrId,
 				url: data.url,
 				extractedId,
-				type: "Youtube"
-
+				type: "Youtube",
+				title: title ?? "Cant find the title",
+				smallImage: tb.standard?.url ?? "https://cdn.dribbble.com/userupload/19072029/file/original-964d527aa2947ce15e717e7bb2f9a749.png",
+				bigImage: tb.maxres?.url ?? "https://cdn.dribbble.com/userupload/19072029/file/original-964d527aa2947ce15e717e7bb2f9a749.png"
 			}
 		})
 		return NextResponse.json({sucess: true, id: stream.id, message: "Added Stream by the user"}, {status: 201})
 	} catch(e) {
+		console.log(e)
 		return NextResponse.json({
 			message: "Error in creating stream",
 		}, {status: 411})
@@ -50,16 +54,16 @@ export async function POST(req: NextRequest) {
 
 
 export async function  GET(req: NextRequest) {
-	const createrId = req.nextUrl.searchParams.get("createrId");
-	const streams = await prismaClient.stream.findMany({
-		where: {
-			userId: createrId ?? ''
-		}
-	})
-
-	return NextResponse.json({
-		streams
-	},{
-		status: 200
-	})
+	try {
+		const createrId = req.nextUrl.searchParams.get("createrId");
+		const streams = await prismaClient.stream.findMany({
+			where: {
+				userId: createrId ?? ''
+			}
+		})
+		return NextResponse.json({streams},{status: 200})
+	} catch (error) {
+		return NextResponse.json({message: "Error in getting stream",}, {status: 411})
+	}
 }
+
